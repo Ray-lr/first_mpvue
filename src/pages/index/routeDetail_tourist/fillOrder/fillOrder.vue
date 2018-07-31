7fc3bacce10fdb744f72df9a16b69d4525c6affd
shy <930053055@qq.com>
2018-07-31 18:07:15 +0800

    预定页面选择营员及预定房间内容新增

--- src/pages/index/routeDetail_tourist/fillOrder/fillOrder.vue
+++ src/pages/index/routeDetail_tourist/fillOrder/fillOrder.vue
@@ -24,7 +24,7 @@
         <div class="manLeft">成人</div>
         <div class="manRight">
           <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;" @click="decreaseBtn">-</button>
-          <input id="manNum" type="number" min="0" value="0" class="numInp"/>
+          <input id="manNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
           <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
         </div>
       </div>
@@ -32,7 +32,7 @@
         <div class="manLeft">儿童</div>
         <div class="manRight">
           <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;">-</button>
-          <input id="childNum" type="number" min="0" value="0" class="numInp"/>
+          <input id="childNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
           <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
         </div>
       </div>
@@ -42,16 +42,67 @@
         <li>
           <div class="weui-flex js-category" >
             <div class="weui-flex-item" >选择营员（多选）</div>
-            <i class="icon icon-74"></i>
+            <div style="padding-right: 30rpx" id="srcIcon"><img :src="src" class="srcIcon" @click="changeList"/></div>
           </div>
-          <div v-for="item in campers" :key="index" style="padding-top: 26rpx;">
-            <div class="camperMember">
-              <p class="camperName">{{item.name}}</p>
-              <p class="camperOlder">({{item.older}})</p>
-            </div>
+          <div class="page" id="camperList">
+            <checkbox-group @change="checkboxChange">
+              <label class="weui-cell weui-check__label" v-for="item in campers" :key="index">
+                <checkbox class="weui-check" :value="item.value" :checked="item.checked" />
+                <div class="weui-cell__bd" style="display: flex">
+                  <p class="camperName">{{item.name}}</p>
+                  <p class="camperOlder">({{item.older}})</p>
+                </div>
+                <div class="weui-cell__hd weui-check__hd_in-checkbox" v-if="item.checked">
+                  <!--<icon class="weui-icon-checkbox_circle" type="circle" size="23" v-if="!item.checked"></icon>-->
+                  <!--<icon class="weui-icon-checkbox_success" type="success" size="23" v-if="item.checked"></icon>-->
+                  <icon class="weui-icon-radio" type="success_no_circle" size="16"></icon>
+                </div>
+              </label>
+            </checkbox-group>
           </div>
+          <!--<div v-for="item in campers" :key="index" style="padding-top: 26rpx;">-->
+            <!--<div class="camperMember">-->
+              <!--<p class="camperName">{{item.name}}</p>-->
+              <!--<p class="camperOlder">({{item.older}})</p>-->
+            <!--</div>-->
+          <!--</div>-->
         </li>
       </ul>
+    </div>
+    <div class="outOrder">
+      <p class="orderNum">预定房间</p>
+      <div class="innerItem">
+        <div class="manLeft">双标</div>
+        <div class="manRight">
+          <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;">-</button>
+          <input id="manNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
+          <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
+        </div>
+      </div>
+      <div class="innerItem">
+        <div class="manLeft">大床</div>
+        <div class="manRight">
+          <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;">-</button>
+          <input id="childNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
+          <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
+        </div>
+      </div>
+      <div class="innerItem">
+        <div class="manLeft">三标</div>
+        <div class="manRight">
+          <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;">-</button>
+          <input id="childNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
+          <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
+        </div>
+      </div>
+      <div class="innerItem">
+        <div class="manLeft">家庭房</div>
+        <div class="manRight">
+          <button type="button" class="decBtn" style="border-right: 1rpx solid #c7c7c7;">-</button>
+          <input id="childNum" type="number" disabled="true" min="0" value="0" class="numInp"/>
+          <button type="button" class=" decBtn addBtn" style="border-left:1rpx solid #c7c7c7;">+</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -70,8 +121,12 @@ export default {
         '6666'
       ],
       campers: [
-        {name: '江小白', older: '儿童'}, {name: '江爸爸', older: '成人'}, {name: '江妈妈', older: '成人'}
-      ]
+        {name: '江小白', older: '儿童', value: '0', checked: false},
+        {name: '江爸爸', older: '成人', value: '1', checked: false},
+        {name: '江妈妈', older: '成人', value: '2', checked: false}
+      ],
+      src: 'http://pics.ctripfair.com/icon_more_def.png',
+      type: false
     }
   },
   mounted: function () {
@@ -79,6 +134,22 @@ export default {
     _this.order.title = _this.$root.$mp.query.title
   },
   methods: {
+    checkboxChange: function (e) {
+      // console.log('checkbox发生change事件，携带value值为：' + e.mp.detail.value)
+      let campers = this.campers
+      let values = e.mp.detail.value
+      for (let i = 0; i < campers.length; ++i) {
+        campers[i].checked = false
+        for (let j = 0; j < values.length; ++j) {
+          if (campers[i].value === values[j]) {
+            campers[i].checked = true
+          }
+        }
+      }
+      this.campers = campers
+    },
+    changeList: function () {
+    },
     decreaseBtn: function () {
     }
   }
@@ -162,7 +233,7 @@ export default {
     margin: 28rpx auto auto 28rpx;
   }
   .manLeft{
-    width: 74rpx;
+    width: 100rpx;
     height: 36rpx;
     font-family: PingFangSC-Regular;
     font-size: 32rpx;
@@ -220,10 +291,9 @@ export default {
     background: #FFFFFF;
     box-shadow: 0 10rpx 36rpx 0 rgba(0,0,0,0.12);
     border-radius: 16rpx;
-    margin-left: 32rpx;
-    margin-right: 32rpx;
+    margin: auto 32rpx 20rpx 32rpx;
     padding-top: 10rpx;
-    padding-bottom: 20rpx;
+    padding-bottom: 30rpx;
   }
   .weui-flex {
     font-family: PingFangSC-Medium;
@@ -231,7 +301,7 @@ export default {
     color: #333333;
     letter-spacing: 0;
     line-height: 36rpx;
-    margin: 28rpx auto 28rpx 28rpx;
+    margin: 28rpx auto 28rpx 20rpx;
   }
   .weui-flex-item{
     -webkit-flex: 1 1 0;
@@ -262,8 +332,14 @@ export default {
     width: 110rpx;
     height: 40rpx;
     font-family: PingFangSC-Regular;
-    font-size: 14px;
+    font-size: 28rpx;
     color: #999999;
-    letter-spacing: -0.41px;
+    letter-spacing: -0.82rpx;
+  }
+  .srcIcon{
+    width: 46rpx;
+    height: 36rpx;
+    float: right;
+    margin-left: 20rpx;
   }
 </style>
