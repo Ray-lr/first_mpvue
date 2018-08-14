f8bddc9c43ec685257e7c32601fa55b0fd5ec10f
lirui1123211 <1027050822@qq.com>
2018-08-14 18:24:39 +0800

    营员添加表单验证

--- src/pages/mine/campusManagement/campusAdd/campusAdd.vue
+++ src/pages/mine/campusManagement/campusAdd/campusAdd.vue
@@ -14,7 +14,9 @@
             <p class="fonts">成员姓名&nbsp&nbsp</p>
           </div>
           <div>
-            <input name="name" v-model="name" style="height: 80rpx;" type="text" placeholder="请输入成员姓名"/>
+            <input name="name" v-model="name" @blur="checkName" style="height: 80rpx;" type="text" placeholder="请输入成员姓名"/>
+            <p class="checkError" v-if="nameCheck!==''">{{nameCheck}}</p>
+            <p class="checked" v-else>姓名认证通过</p>
           </div>
         </div>
         <div class="element" style="border-style: solid;border-width: 2rpx 0;border-color: #c7c7c7">
@@ -26,7 +28,9 @@
             </picker>
           </div>
           <div>
-            <input name="idNum" v-model="idNum" style="height: 80rpx;" type="idcard" placeholder="请输入证件号码"/>
+            <input name="idNum" v-model="idNum" @blur="checkID" style="height: 80rpx;" type="idcard" placeholder="请输入证件号码"/>
+            <p class="checkError" v-if="idNumCheck!==''">{{idNumCheck}}</p>
+            <p class="checked" v-else>身份证号认证通过</p>
           </div>
         </div>
         <div class="element">
@@ -36,7 +40,9 @@
             </p>
           </picker>
           <div>
-            <input name="phone" v-model="phone" style="height: 80rpx;" type="number" placeholder="请输入联系方式"/>
+            <input name="phone" v-model="phone" @blur="checkPhone" style="height: 80rpx;" type="number" placeholder="请输入联系方式"/>
+            <p class="checkError" v-if="phoneCheck!==''">{{phoneCheck}}</p>
+            <p class="checked" v-else>手机号认证通过</p>
           </div>
         </div>
         <div>
@@ -56,17 +62,47 @@
     },
     data () {
       return {
+        IDRex: /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/,
         indexPicker: 0,
         array_role: ['儿子', '女儿'],
         array_id: ['身份证', '港澳台通行证', '护照', '军官证'],
         idType: '身份证',
         role: '家庭角色',
         name: '',
+        nameCheck: ' ',
         idNum: '',
-        phone: ''
+        idNumCheck: ' ',
+        phone: '',
+        phoneCheck: ' '
       }
     },
     methods: {
+      checkName () {
+        let nameRex = /^[\u4E00-\u9FA5\uf900-\ufa2d·s]{2,20}$/
+        let result = nameRex.test(this.name)
+        if (result) {
+          this.nameCheck = ''
+        } else {
+          this.nameCheck = '请输入正确的姓名'
+        }
+      },
+      checkID () {
+        let result = this.IDRex.test(this.idNum)
+        if (result) {
+          this.idNumCheck = ''
+        } else {
+          this.idNumCheck = '请输入正确的身份证号码'
+        }
+      },
+      checkPhone () {
+        let nameRex = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/
+        let result = nameRex.test(this.phone)
+        if (result) {
+          this.phoneCheck = ''
+        } else {
+          this.phoneCheck = '请输入正确的手机号码'
+        }
+      },
       roleChange (e) {
         console.log('选中的值为：' + this.array_role[e.mp.detail.value])
         this.role = this.array_role[e.mp.detail.value]
@@ -74,10 +110,35 @@
       IDChange (e) {
         this.idType = this.array_id[e.mp.detail.value]
         this.indexPicker = e.mp.detail.value
+        switch (this.idType) {
+          case '港澳台通行证':
+            this.IDRex = /(^[HMhm]{1}([0-9]{10}|[0-9]{8})$)|(^[0-9]{8}$)|(^[0-9]{10}$)/
+            break
+          case '护照':
+            this.IDRex = /(^[a-zA-Z]{5,17}$)|(^[a-zA-Z0-9]{5,17}$)/
+            break
+          case '军官证':
+            this.IDRex = /^[a-zA-Z0-9]{7,21}$/
+        }
         console.log('选中的证件类型为：' + this.array_id[e.mp.detail.value])
       },
       addSubmit: function (event) {
-        console.log('姓名：' + this.name + '身份证号：' + this.idNum + '手机号：' + this.phone)
+        if (this.nameCheck === '' && this.idNumCheck === '' && this.phoneCheck === '') {
+          console.log('姓名：' + this.name + '身份证号：' + this.idNum + '手机号：' + this.phone)
+        } else {
+          wx.showModal({
+            title: '错误',
+            content: '信息有误，请重新填写',
+            confirmText: '确定',
+            showCancel: false,
+            // cancelText: '取消',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('确定')
+              }
+            }
+          })
+        }
       }
     }
   }
@@ -112,5 +173,15 @@
     height: 120rpx;
     margin: 60rpx 314rpx;
     background-image: url(http://pics.ctripfair.com/ico_add1.png);
+  }
+  .checked{
+    font-size: small;
+    font-weight: bold;
+    color: green
+  }
+  .checkError{
+    font-size: small;
+    font-weight: bold;
+    color: red
   }
 </style>
